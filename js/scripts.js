368bb7b4ee6ab5bf8052a64a4c8037fda526b4d2
David Darnes <me@daviddarnes.com>
2015-09-18 14:21:55 +0100

    Is this right?

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 // Scrambled email
-var emaillink = document.querySelectorAll("[title=my email]");
+var emaillink = document.querySelectorAll('[title="my email"]');
 var email = 'me' + '@' + 'daviddarnes' + '.' + 'com';
 emaillink.setAttribute('href', 'mailto:' + email);

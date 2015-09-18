4d4d11680c4d754d9d3cd7b6f08d438daf6d5502
David Darnes <me@daviddarnes.com>
2015-09-18 14:09:39 +0100

    Adds email to link using js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,4 @@
-// Nothing yet
+// Scrambled email
+var contactform = document.getElementsByTagName('my email');
+var email = 'me' + '@' + 'daviddarnes' + '.' + 'com';
+contactform.setAttribute('href', 'mailto:' + email);
